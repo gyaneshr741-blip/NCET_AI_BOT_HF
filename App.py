5605aa43b725a9889fad5b8e0f99aca4ecb1f4fc
gyaneshr741-blip <gyaneshr741@gmail.com>
2026-04-15 15:10:35 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import pipeline
 @st.cache_resource
 def load_summarizer():
-  return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+  return pipeline("Summarization", model="sshleifer/distilbart-cnn-12-6")
 
 summarizer = load_summarizer()
 #streamlit UI
